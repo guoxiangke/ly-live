ea27270a872313260fa080c2d759d07011c31b3a
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2022-08-31 16:42:47 -0700

    up

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">第五空间</ion-title>
         </ion-toolbar>
       </ion-header>
       <iframe src="https://www4.cbox.ws/box/?boxid=4327572&amp;boxtag=Cf5HyA&amp;tid=1&amp;tkey=ce16b6434b5e5b3b" marginwidth="0" marginheight="0" scrolling="auto" width="100%" height="100%" frameborder="0"></iframe>
--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">节目时间</ion-title>
         </ion-toolbar>
       </ion-header>
       <iframe src="https://ly729.airtime.pro/embed/weekly-program" 
