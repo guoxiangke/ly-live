9ca94d49996c178276ac8033c73611d3608ab5a7
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2022-09-05 21:53:44 -0700

    random srcs

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -14,9 +14,9 @@
       <div style="text-align: center; padding: 4em;">
         <!-- https://ly729.airtime.pro/api/live-info -->
         <audio controls="controls" autoplay preload="true">
-          <source src="https://ly729.out.airtime.pro/ly729_a" type="audio/mpeg">
-          <source src="https://ly729.out.airtime.pro/ly729_b" type="audio/mp3">
-          <source src="https://listen.729ly.net:8001/ly729_a" type="audio/mp3">
+          <source :src="urls[0]" type="audio/mpeg">
+          <source :src="urls[1]" type="audio/mpeg">
+          <source :src="urls[2]" type="audio/mpeg">
         </audio>
       </div>
     </ion-content>
@@ -29,6 +29,15 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: {  IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {  IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data() {
+    let urls = ['https://ly729.out.airtime.pro/ly729_a','https://ly729.out.airtime.pro/ly729_b','https://listen.729ly.net:8001/ly729_a']
+    urls = urls.sort(() => Math.random() - 0.5);
+    return {
+      urls
+    }
+  }
 });
+
+
 </script>
