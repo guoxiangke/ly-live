eeb5066a1185c5bf8c23525ea5f1aa1467e86648
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2022-11-30 19:46:32 -0800

    alt

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,17 +4,17 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
+          <ion-icon  alt="同行频道" :title="同行频道" :icon="triangle" />
           <ion-label alt="同行频道">同行频道</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
+          <ion-icon  alt="第五空间" :title="第五空间" :icon="ellipse" />
           <ion-label alt="第五空间">第五空间</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon  alt="节目时间" :title="节目时间" :icon="square" />
           <ion-label alt="节目时间">节目时间</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
